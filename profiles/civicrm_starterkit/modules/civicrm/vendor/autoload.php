8104adc2144d08e127a0219e4e29bf8f86887052
Herb vd Dool <herb@3speedhub.com>
2019-05-16 12:24:38 -0400

    Update to CiviCRM 5.13.4. This is a security release and includes bug fixes. Please upgrade immediately.
    Read more about the changes: https://github.com/civicrm/civicrm-core/blob/master/release-notes/5.13.4.md.
    
    Make sure you backup using Pantheon's backup tool first. Fully test on a dev environment before upgrading on live.
    
    If you have questions contact https://civicrmstarterkit.org/contact. We provide some basic general support for the public. If you require help with your specific website there will likely be a cost.

--- profiles/civicrm_starterkit/modules/civicrm/vendor/autoload.php
+++ profiles/civicrm_starterkit/modules/civicrm/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit285dea4ecfc38f35aab901dfc1c31a29::getLoader();
+return ComposerAutoloaderInit87ee1b297ddac2c843864f7c2d4f758f::getLoader();
